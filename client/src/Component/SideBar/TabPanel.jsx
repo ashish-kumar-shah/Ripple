6886d25df9f0bbadc39dc9f0dbee89560d82908c
Ashish kumar shah <ash.del.013@gmail.com>
2025-07-02 14:39:28 +0530

    ui error fixed

--- client/src/Component/SideBar/TabPanel.jsx
+++ client/src/Component/SideBar/TabPanel.jsx
@@ -32,11 +32,9 @@ const TabPanel = () => {
           <div
             key={tab.name}
             onClick={() => navigate(tab.url)}
-            className={`
-              cursor-pointer px-3 py-2 rounded flex flex-col items-center
-              transition-all duration-200 ease-in-out
-              ${isActive ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"}
-            `}
+            className={`relative cursor-pointer px-3 py-2 rounded flex flex-col items-center transition-all duration-200 ease-in-out ${
+              isActive ? "bg-blue-100 text-blue-600" : "hover:bg-gray-100"
+            }`}
           >
             <span
               className="material-symbols-outlined"
@@ -44,17 +42,15 @@ const TabPanel = () => {
             >
               {tab.icon}
             </span>
-            {/*  */}
+
             {tab.icon === "notifications" &&
               notification !== null &&
-              notification.length >
-                0(
-                  <div className="  absolute -translate-x-4 -translate-y-1.5 font-bold   bg-red-600 text-center w-4 h-4 rounded-full flex justify-center items-center text-white text-xs">
-                    {notification.length}
-                  </div>
-                )}
+              notification.length > 0 && (
+                <div className="absolute top-0 right-2 font-bold bg-red-600 w-4 h-4 rounded-full flex justify-center items-center text-white text-xs">
+                  {notification.length}
+                </div>
+              )}
 
-            {/*  */}
             <span className="text-sm font-medium">{tab.name}</span>
           </div>
         );
